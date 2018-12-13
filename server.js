73bb82acace6408e80756fd6e402036389ff5138
Damion White <damion.dewit@outlook.com>
2018-12-13 11:08:37 +0100

    set port to env variable or 5000

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function (req, res) {
-  console.log(`listening on port 3000`);
+app.listen(app.get('port'), function (req, res) {
+  console.log(`listening on port ${app.get('port')}`);
 });
