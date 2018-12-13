ae92184aa64311fc318d381bc15a077c5c231ba4
Damion White <damion.dewit@outlook.com>
2018-12-13 11:11:37 +0100

    Small edits to work better with the dist workflow

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: path.join(__dirname, '/src/index.js'),
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, '/dist')
+    path: path.join(__dirname, '/dist/public')
   },
   module: {
     rules: [
